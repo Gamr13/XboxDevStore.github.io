55371a7975f091b8761a0db00a894645b50c6b4e
Gamr13 <mortals68@gmail.com>
2024-03-12 23:06:40 +0000

    Progress on guide page & Add Language selection

--- scripts/script.js
+++ scripts/script.js
@@ -1,10 +1,10 @@
-  document.addEventListener('DOMContentLoaded', function () {
+document.addEventListener('DOMContentLoaded', function () {
     const contentSection = document.querySelector('section.intro'); // Selects the content section
 
     // Map all the content to their sections (should also map sections to JSON dicts for easier adding/removing)
     const contentMap = {
         "Introduction": "<h2>Intro</h2> <p>Intro text.</p>",
-        "Section 2": "<h2>Section 2</h2> <p>Section 2 text.</p>",
+        "Test 2": "<h2>Section 2</h2> <p>Section 2 text.</p>",
         "Section 3": "<h2>Section 3</h2> <p>Section 3 text.</p>",
         "Section 4": "<h2>Section 4</h2> <p>Section 4 text.</p>",
         "Section 5": "<h2>Section 5</h2> <p>Section 5 text.</p>",
@@ -12,10 +12,14 @@
         "Section 7": "<h2>Section 7</h2> <p>Section 7 text.</p>",
     };
 
-    // Function to update main content
-    function updateMainContent(sectionName) {
+    function updateMainContentAndSidebar(sectionName) {
         const htmlContent = contentMap[sectionName] || "<h2>Not Available</h2><p>No content available for this section.</p>"; // Default message if sectionName not found
         contentSection.innerHTML = htmlContent; // Replaces the innerHTML of the content section
+        
+        sidebarLinks.forEach(link => {
+            link.classList.remove('active');
+        });
+        this.classList.add('active');
     }
 
     const sidebarLinks = document.querySelectorAll('.list-group-item-action');
@@ -24,7 +28,24 @@
             e.preventDefault(); // Prevents the default link behavior
 
             const sectionName = this.querySelector('span').textContent; // Gets the section name from the span element
-            updateMainContent(sectionName); // Updates the content section based on the clicked section using the content map
+            updateMainContentAndSidebar.call(this, sectionName); // Updates the content section based on the clicked section using the content map
         });
     });
 });
+
+function myFunction() {
+    document.getElementById("myDropdown").classList.toggle("show");
+}
+
+window.onclick = function(event) {
+    if (!event.target.matches('.dropbtn')) {
+      var dropdowns = document.getElementsByClassName("dropdown-content");
+      var i;
+      for (i = 0; i < dropdowns.length; i++) {
+        var openDropdown = dropdowns[i];
+        if (openDropdown.classList.contains('show')) {
+          openDropdown.classList.remove('show');
+        }
+      }
+    }
+  }
